8dc820b15a06d7538dbe285038e0bdcd9a200a6e
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2021-11-01 01:57:28 -0600

    GREEN: Delete City functionality (should work)

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -67,5 +67,12 @@ public class CustomList extends ArrayAdapter<City> {
 
     public void deleteCity(City city){
 
+        if (cities.contains(city)){
+            cities.remove(city);
+        }
+        else{
+            throw new IllegalArgumentException("Doesn't exist");
+        }
+
     }
 }
